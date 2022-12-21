36f88f99c49da969765cf6222d91b2eb441e3251
PTMC7216 <94150909+PTMC7216@users.noreply.github.com>
2022-12-21 01:42:25 -0800

    Switch to CTk and refactor

--- main.py
+++ main.py
@@ -1,116 +1,90 @@
+import customtkinter
 import tkinter as tk
 from tkinter import ttk
 from functools import partial
 import sqlite3
 
 
-class GUI(tk.Tk):
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+
+class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+
         conn = sqlite3.connect('resources/eft.db')
         self.c = conn.cursor()
 
         self.title('EFT Munitions')
         self.iconbitmap('resources/ammo.ico')
+        self.resizable(False, False)
+        win_w, win_h = 800, 567
 
-        self.resizable(0, 0)
-        window_width = 830
-        window_height = 520
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        x_coord = int((screen_width / 2) - (window_width / 2))
-        y_coord = int((screen_height / 2) - (window_height / 2) - 40)
-
-        self.radio = None
-        self.results = None
-        self.ammo_tree = None
-        self.weapon_tree = None
-
-        self.widgets()
-
-        self.attributes('-topmost', True)
-        self.geometry(f"{window_width}x{window_height}+{x_coord}+{y_coord}")
-        self.update()
-        self.attributes('-topmost', False)
-
-    def widgets(self):
-        pad = {'padx': 10, 'pady': 10}
-        self.search_frame(pad)
-        self.button_frame(pad)
-        self.db_frame(pad)
-
-    def search_frame(self, pad):
-        search_frame = ttk.Frame(self)
-        search_frame.grid(column=0, row=0, **pad)
-        ttk.Label(search_frame, text='Search by name: ').grid(column=0, row=0)
-
-        self.radio = tk.IntVar()
-        self.radio.set(0)
-        ttk.Radiobutton(search_frame, text='Ammo', variable=self.radio, value=0).grid(column=1, row=0)
-        ttk.Radiobutton(search_frame, text='Weapon', variable=self.radio, value=1).grid(column=2, row=0)
-        ttk.Button(search_frame, text='Submit', command=self.submit_click).grid(column=2, row=1)
-
-        self.results = ttk.Label(search_frame, text='')
-        self.results.grid(column=0, row=2, columnspan=3)
-
-    def button_frame(self, pad):
-        btn_frame = ttk.Frame(self)
-        btn_frame.grid(column=0, row=1, padx=10)
-        ttk.Label(btn_frame, text='Search by cartridge:').grid(column=0, row=0, columnspan=2)
-
-        rifle_frame = ttk.Frame(btn_frame)
-        rifle_frame.grid(column=0, row=1, rowspan=2, sticky='nw', **pad)
-        ttk.Label(rifle_frame, text='Rifle').grid(sticky='w')
-        rifle_cartridges = ['9x39mm', '.366 TKM', '5.45x39mm', '5.56x45mm NATO', '.300 Blackout', '7.62x39mm',
-                            '7.62x51mm NATO', '7.62x54mmR', '.338 Lapua Magnum', '12.7x55mm STs-130']
-        for cartridge in rifle_cartridges:
-            ttk.Button(rifle_frame, text=cartridge, command=partial(self.ammo_click, cartridge)).grid(sticky='w')
-
-        gl_frame = ttk.Frame(btn_frame)
-        gl_frame.grid(column=0, row=3, sticky='sw', **pad)
-        ttk.Label(gl_frame, text='GL').grid(sticky='w')
-        gl_cartridges = ['40x46 mm']
-        for cartridge in gl_cartridges:
-            ttk.Button(gl_frame, text=cartridge, command=partial(self.ammo_click, cartridge)).grid(sticky='w')
-
-        shotgun_frame = ttk.Frame(btn_frame)
-        shotgun_frame.grid(column=1, row=1, sticky='w', **pad)
-        ttk.Label(shotgun_frame, text='Shotgun').grid(sticky='w')
-        shotgun_cartridges = ['12/70', '20/70', '23x75mm']
-        for cartridge in shotgun_cartridges:
-            ttk.Button(shotgun_frame, text=cartridge, command=partial(self.ammo_click, cartridge)).grid(sticky='w')
-
-        pistol_frame = ttk.Frame(btn_frame)
-        pistol_frame.grid(column=1, row=2, sticky='w', **pad)
-        ttk.Label(pistol_frame, text='Pistol').grid(sticky='w')
-        pistol_cartridges = ['7.62x25mm Tokarev', '9x18mm Makarov', '9x19mm Parabellum', '9x21mm Gyurza', '.45 ACP']
-        for cartridge in pistol_cartridges:
-            ttk.Button(pistol_frame, text=cartridge, command=partial(self.ammo_click, cartridge)).grid(sticky='w')
-
-        pdw_frame = ttk.Frame(btn_frame)
-        pdw_frame.grid(column=1, row=3, sticky='w', **pad)
-        ttk.Label(pdw_frame, text='PDW').grid(sticky='w')
-        pdw_cartridges = ['4.6x30mm HK', '5.7x28mm FN']
-        for cartridge in pdw_cartridges:
-            ttk.Button(pdw_frame, text=cartridge, command=partial(self.ammo_click, cartridge)).grid(sticky='w')
-
-    def db_frame(self, pad):
-        db_frame = ttk.Frame(self)
-        db_frame.grid(column=1, row=0, rowspan=2, sticky='n', **pad)
-
-        scroll_height = {'ipady': 88}
-
-        ammo_tree_f = ttk.LabelFrame(db_frame, text="Ammunition")
-        ammo_tree_f.grid(column=0, row=0)
-        ammo_tree_scroll = ttk.Scrollbar(ammo_tree_f)
-        ammo_tree_scroll.grid(column=1, row=0, **scroll_height)
-        self.ammo_tree = TvSort(ammo_tree_f,
-                                columns=("Cartridge", "Name", "Dmg", "Pen", "Frag"),
-                                yscrollcommand=ammo_tree_scroll.set)
-
-        self.entry = ttk.Entry(search_frame)
-        self.entry.grid(column=0, row=1, columnspan=2, ipadx=15, pady=10)
+        self.search_frame = customtkinter.CTkFrame(self)
+        self.search_frame.grid(column=0, row=0, padx=5, pady=5, sticky='n')
+        self.search_label = customtkinter.CTkLabel(self.search_frame, text='Search by name: ')
+        self.search_label.grid(column=0, columnspan=3, row=0)
+        self.search_subframe = customtkinter.CTkFrame(self.search_frame)
+        self.search_subframe.grid(column=0, row=1, padx=4)
+        self.radio_var = tk.IntVar()
+        self.radio_var.set(0)
+        radio_size = {'radiobutton_width': 20, 'radiobutton_height': 20, 'border_width_unchecked': 3, 'border_width_checked': 6}
+        self.radio_button_0 = customtkinter.CTkRadioButton(self.search_subframe, text='Ammo', variable=self.radio_var, value=0, **radio_size)
+        self.radio_button_0.grid(column=0, row=0, pady=8)
+        self.radio_button_1 = customtkinter.CTkRadioButton(self.search_subframe, text='Weapon', variable=self.radio_var, value=1, **radio_size)
+        self.radio_button_1.grid(column=1, row=0, pady=8)
+        self.entry = customtkinter.CTkEntry(self.search_subframe)
+        self.entry.grid(column=0, row=1, padx=8, ipadx=9, pady=(0, 8))
         self.entry.bind('<Return>', self.submit_entry)
+        self.submit_button = customtkinter.CTkButton(self.search_subframe, text='Submit', command=self.submit_entry, width=100)
+        self.submit_button.grid(column=1, row=1, padx=(0, 8), pady=(0, 8))
+        self.results_label = customtkinter.CTkLabel(self.search_frame, text='')
+        self.results_label.grid(column=0, row=3, columnspan=3)
+
+        self.cartridge_frame = customtkinter.CTkFrame(self)
+        self.cartridge_frame.grid(column=0, row=1, padx=5, sticky='n')
+        self.cartridge_label = customtkinter.CTkLabel(self.cartridge_frame, text='Search by cartridge:')
+        self.cartridge_label.grid(column=0, columnspan=2, row=0)
+        self.rifle_frame = customtkinter.CTkFrame(self.cartridge_frame)
+        self.rifle_frame.grid(column=0, row=1, rowspan=3, sticky='n', padx=4, pady=(0, 4))
+        self.rifle_label = customtkinter.CTkLabel(self.rifle_frame, text='Rifle')
+        self.rifle_label.grid()
+        self.gl_frame = customtkinter.CTkFrame(self.cartridge_frame)
+        self.gl_frame.grid(column=0, row=3, sticky='s', padx=4, pady=4)
+        self.gl_label = customtkinter.CTkLabel(self.gl_frame, text='GL')
+        self.gl_label.grid()
+        self.shotgun_frame = customtkinter.CTkFrame(self.cartridge_frame)
+        self.shotgun_frame.grid(column=1, row=1, padx=(0, 5), pady=(0, 4))
+        self.shotgun_label = customtkinter.CTkLabel(self.shotgun_frame, text='Shotgun')
+        self.shotgun_label.grid()
+        self.pistol_frame = customtkinter.CTkFrame(self.cartridge_frame)
+        self.pistol_frame.grid(column=1, row=2, padx=(0, 5), pady=4)
+        self.pistol_label = customtkinter.CTkLabel(self.pistol_frame, text='Pistol')
+        self.pistol_label.grid()
+        self.pdw_frame = customtkinter.CTkFrame(self.cartridge_frame)
+        self.pdw_frame.grid(column=1, row=3, padx=(0, 5), pady=4)
+        self.pdw_label = customtkinter.CTkLabel(self.pdw_frame, text='PDW')
+        self.pdw_label.grid()
+        self.cartridge_frame_map = {
+            self.rifle_frame: ['9x39mm', '.366 TKM', '5.45x39mm', '5.56x45mm NATO', '.300 Blackout', '7.62x39mm',
+                               '7.62x51mm NATO', '7.62x54mmR', '.338 Lapua Magnum', '12.7x55mm STs-130'],
+            self.gl_frame: ['40x46 mm'],
+            self.shotgun_frame: ['12/70', '20/70', '23x75mm'],
+            self.pistol_frame: ['7.62x25mm Tokarev', '9x18mm Makarov', '9x19mm Parabellum', '9x21mm Gyurza', '.45 ACP'],
+            self.pdw_frame: ['4.6x30mm HK', '5.7x28mm FN']
+        }
+        for frame, cartridges in self.cartridge_frame_map.items():
+            for cartridge in cartridges:
+                customtkinter.CTkButton(frame, text=cartridge, command=partial(self.submit_cartridge, cartridge),
+                                        width=130, height=25).grid(padx=4, pady=(0, 4))
+
+        self.ammo_tree_frame = customtkinter.CTkFrame(self)
+        self.ammo_tree_frame.grid(column=1, row=0, rowspan=2, sticky='n', pady=5)
+        self.ammo_tree_label = customtkinter.CTkLabel(self.ammo_tree_frame, text="Ammunition")
+        self.ammo_tree_label.grid(column=0, row=0)
+        self.ammo_tree = CustomTreeview(self.ammo_tree_frame, height=11, columns=("Cartridge", "Name", "Dmg", "Pen", "Frag"))
+        self.ammo_tree.grid(column=0, row=1)
         self.ammo_tree.column("#0", width=0, stretch=0)
         self.ammo_tree.column("Cartridge", **self.get_width_kwargs(120))
         self.ammo_tree.column("Name", **self.get_width_kwargs(210))
@@ -122,17 +96,18 @@ class GUI(tk.Tk):
         self.ammo_tree.heading("Dmg", text="Dmg", sort_by="x")
         self.ammo_tree.heading("Pen", text="Pen", sort_by="number")
         self.ammo_tree.heading("Frag", text="Frag", sort_by="percent")
-        self.ammo_tree.grid(column=0, row=0)
-        ammo_tree_scroll.config(command=self.ammo_tree.yview)
+        self.ammo_tree_scroll = customtkinter.CTkScrollbar(self.ammo_tree_frame, command=self.ammo_tree.yview)
+        self.ammo_tree_scroll.grid(column=1, row=1, ipady=24)
+        self.ammo_tree.configure(yscrollcommand=self.ammo_tree_scroll.set)
+        self.ammo_tree.bind('<Button-1>', self.separator_lock)
+        self.ammo_tree.bind('<Motion>', self.separator_lock)
 
-        weapon_tree_f = ttk.LabelFrame(db_frame, text="Weaponry")
-        weapon_tree_f.grid(column=0, row=1)
-        weapon_tree_scroll = ttk.Scrollbar(weapon_tree_f)
-        weapon_tree_scroll.grid(column=1, row=0, **scroll_height)
-        self.weapon_tree = TvSort(weapon_tree_f,
-                                  columns=("Cartridge", "Name", "Type", "Recoil", "Ergo", "RPM"),
-                                  yscrollcommand=weapon_tree_scroll.set)
-
+        self.weapon_tree_frame = customtkinter.CTkFrame(self)
+        self.weapon_tree_frame.grid(column=1, row=1, sticky='s')
+        self.weapon_tree_label = customtkinter.CTkLabel(self.weapon_tree_frame, text="Weaponry")
+        self.weapon_tree_label.grid(column=0, row=0)
+        self.weapon_tree = CustomTreeview(self.weapon_tree_frame, height=11, columns=("Cartridge", "Name", "Type", "Recoil", "Ergo", "RPM"))
+        self.weapon_tree.grid(column=0, row=1)
         self.weapon_tree.column("#0", width=0, stretch=0)
         self.weapon_tree.column("Cartridge", **self.get_width_kwargs(120))
         self.weapon_tree.column("Name", **self.get_width_kwargs(120))
@@ -146,17 +121,23 @@ class GUI(tk.Tk):
         self.weapon_tree.heading("Recoil", text="Recoil", sort_by="number")
         self.weapon_tree.heading("Ergo", text="Ergo", sort_by="number")
         self.weapon_tree.heading("RPM", text="RPM", sort_by="number")
-        self.weapon_tree.grid(column=0, row=0)
-        weapon_tree_scroll.config(command=self.weapon_tree.yview)
+        self.weapon_tree_scroll = customtkinter.CTkScrollbar(self.weapon_tree_frame, command=self.weapon_tree.yview)
+        self.weapon_tree_scroll.grid(column=1, row=1, ipady=24)
+        self.weapon_tree.configure(yscrollcommand=self.weapon_tree_scroll.set)
+        self.weapon_tree.bind('<Button-1>', self.separator_lock)
+        self.weapon_tree.bind('<Motion>', self.separator_lock)
 
-        self.ammo_tree.bind('<Button-1>', self.separator_resize_blocker)
-        self.ammo_tree.bind('<Motion>', self.separator_resize_blocker)
-        self.weapon_tree.bind('<Button-1>', self.separator_resize_blocker)
-        self.weapon_tree.bind('<Motion>', self.separator_resize_blocker)
+        win_x = int((self.winfo_screenwidth() / 2) - (win_w / 2))
+        win_y = int((self.winfo_screenheight() / 2) - (win_h / 2) - 40)
+        self.attributes('-topmost', True)
+        self.geometry(f"{win_w}x{win_h}+{win_x}+{win_y}")
+        self.update()
+        self.attributes('-topmost', False)
 
     def count_results(self):
         rows = len(self.ammo_tree.get_children()) + len(self.weapon_tree.get_children())
-        self.results.config(text=f"Found {rows} results")
+        self.results_label.configure(text=f"Found {rows} results")
+
     def execute_and_populate(self, query, tree, col_indices):
         self.c.execute(query)
         for col in self.c.fetchall():
@@ -216,6 +197,10 @@ class GUI(tk.Tk):
     def submit_cartridge(self, cartridge):
         self.query_cartridge(cartridge)
 
+    def separator_lock(self, event):
+        if self.ammo_tree.identify_region(event.x, event.y) == "separator" or \
+                self.weapon_tree.identify_region(event.x, event.y) == "separator":
+            return "break"
 
     @staticmethod
     def get_width_kwargs(width):
@@ -230,17 +215,32 @@ class GUI(tk.Tk):
                 'stretch': 0}
 
 
+class CustomTreeview(ttk.Treeview):
+    def __init__(self, *a, **kw):
+        super().__init__(*a, **kw)
 
+        treestyle = ttk.Style()
+        treestyle.theme_use('default')
 
+        treestyle.configure("Treeview",
+                            fieldbackground=customtkinter.ThemeManager.theme["CTkFrame"]["fg_color"][1],
+                            background=customtkinter.ThemeManager.theme["CTkFrame"]["fg_color"][1],
+                            foreground=customtkinter.ThemeManager.theme["CTkLabel"]["text_color"][1],
+                            borderwidth=0)
 
-    def separator_resize_blocker(self, event):
-        if self.ammo_tree.identify_region(event.x, event.y) == "separator" or \
-                self.weapon_tree.identify_region(event.x, event.y) == "separator":
-            return "break"
+        treestyle.map('Treeview',
+                      background=[('selected', customtkinter.ThemeManager.theme["CTkButton"]["fg_color"][1])],
+                      foreground=[('selected', customtkinter.ThemeManager.theme["CTkButton"]["text_color"][1])])
 
+        treestyle.configure('Treeview.Heading',
+                            background=customtkinter.ThemeManager.theme["CTkButton"]["fg_color"][1],
+                            foreground=customtkinter.ThemeManager.theme["CTkButton"]["text_color"][1],
+                            relief='flat',
+                            borderwidth=6)
 
+        treestyle.map('Treeview.Heading',
+                      background=[('active', customtkinter.ThemeManager.theme["CTkButton"]["hover_color"][1])])
 
-class TvSort(ttk.Treeview):
     def heading(self, column, sort_by=None, anchor='w', **kwargs):
         if sort_by and not hasattr(kwargs, "command"):
             func = getattr(self, f"_sort_by_{sort_by}", None)
@@ -276,9 +276,6 @@ class TvSort(ttk.Treeview):
         self._sort(column, reverse, _percent_to_number, self._sort_by_percent)
 
 
-def main():
-    GUI().mainloop()
-
-
 if __name__ == '__main__':
-    main()
+    app = App()
+    app.mainloop()
